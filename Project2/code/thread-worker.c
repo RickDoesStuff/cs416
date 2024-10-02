1a732a4c32ec8721f1f85b35cc350589d4b52e59
RickDoesStuff <rickyjaquino@gmail.com>
2024-10-02 16:40:29 -0400

    1.1.1 done 1.1.2 almost done

---
+++ Project2/code/thread-worker.c
@@ -0,0 +1,222 @@
+// File:	thread-worker.c
+
+// List all group member's name:
+// username of iLab:
+// iLab Server:
+
+#include "thread-worker.h"
+
+#define STACK_SIZE 1024 * 8 // 8,192 bytes for stack, default stack size
+
+//Global counter for total context switches and 
+//average turn around and response time
+long tot_cntx_switches=0;
+double avg_turn_time=0;
+double avg_resp_time=0;
+
+
+// INITAILIZE ALL YOUR OTHER VARIABLES HERE
+// YOUR CODE HERE
+
+static worker_t next_thread_id = 0;
+
+/* create a new thread */
+int worker_create(worker_t * thread, pthread_attr_t * attr, 
+					void *(*function)(void*), void * arg) {
+
+	// - create Thread Control Block (TCB)
+
+	// allocate the size of the new thread
+	tcb *new_thread = (tcb *)malloc(sizeof(tcb));
+
+	// if not enough memory, throw and error
+	if (new_thread == NULL) {
+		perror("Unable to allocate memory for TCB");
+		exit(1);
+	}
+
+	// get the next thread id
+	new_thread->tid = next_thread_id++;
+	print("thread id is %i\n",new_thread->tid);
+
+	// - allocate space of stack for this thread to run
+	new_thread->stack = malloc(STACK_SIZE);
+	// if not enough memory, throw an error
+	if (new_thread->stack == NULL) {
+		perror("Unable to allocate memory for stack");
+		exit(1);
+	}
+
+
+	// - create and initialize the context of this worker thread
+
+	// get the current context and make sure it was gotten without error
+	if (getcontext(&(new_thread->ctx)) < 0) {
+		perror("getcontext");
+		exit(1);
+	}
+
+	/* Setup context that we are going to use */
+	new_thread->ctx.uc_link = NULL;
+	new_thread->ctx.uc_stack.ss_sp = new_thread->stack;
+	new_thread->ctx.uc_stack.ss_size = STACK_SIZE;
+	new_thread->ctx.uc_stack.ss_flags = 0;
+	
+	print("about to call make context");
+	// setup the context to start running at the given function with given 1 arg *void type
+	makecontext(new_thread, function, 1, arg);
+	print("successfully modified context");
+
+	// after everything is set, push this thread into run queue and 
+	// - make it ready for the execution.
+	// READY status
+	new_thread->status = 0;
+	// Default priority
+	new_thread->priority = 1;
+
+	// returns the tid to the caller
+	*thread = new_thread->tid;
+
+	return 0; // completed
+};
+
+
+#ifdef MLFQ
+/* This function gets called only for MLFQ scheduling set the worker priority. */
+int worker_setschedprio(worker_t thread, int prio) {
+
+
+   // Set the priority value to your thread's TCB
+   // YOUR CODE HERE
+
+   return 0;	
+
+}
+#endif
+
+
+
+/* give CPU possession to other user-level worker threads voluntarily */
+int worker_yield() {
+	
+	// - change worker thread's state from Running to Ready
+	// - save context of this thread to its thread control block
+	// - switch from thread context to scheduler context
+
+	// YOUR CODE HERE
+	
+	return 0;
+};
+
+/* terminate a thread */
+void worker_exit(void *value_ptr) {
+	// - de-allocate any dynamic memory created when starting this thread
+
+	// YOUR CODE HERE
+};
+
+
+/* Wait for thread termination */
+int worker_join(worker_t thread, void **value_ptr) {
+	
+	// - wait for a specific thread to terminate
+	// - de-allocate any dynamic memory created by the joining thread
+  
+	// YOUR CODE HERE
+	return 0;
+};
+
+/* initialize the mutex lock */
+int worker_mutex_init(worker_mutex_t *mutex, 
+                          const pthread_mutexattr_t *mutexattr) {
+	//- initialize data structures for this mutex
+
+	// YOUR CODE HERE
+	return 0;
+};
+
+/* aquire the mutex lock */
+int worker_mutex_lock(worker_mutex_t *mutex) {
+
+        // - use the built-in test-and-set atomic function to test the mutex
+        // - if the mutex is acquired successfully, enter the critical section
+        // - if acquiring mutex fails, push current thread into block list and
+        // context switch to the scheduler thread
+
+        // YOUR CODE HERE
+        return 0;
+};
+
+/* release the mutex lock */
+int worker_mutex_unlock(worker_mutex_t *mutex) {
+	// - release mutex and make it available again. 
+	// - put threads in block list to run queue 
+	// so that they could compete for mutex later.
+
+	// YOUR CODE HERE
+	return 0;
+};
+
+
+/* destroy the mutex */
+int worker_mutex_destroy(worker_mutex_t *mutex) {
+	// - de-allocate dynamic memory created in worker_mutex_init
+
+	return 0;
+};
+
+/* scheduler */
+static void schedule() {
+	// - every time a timer interrupt occurs, your worker thread library 
+	// should be contexted switched from a thread context to this 
+	// schedule() function
+
+	// - invoke scheduling algorithms according to the policy (PSJF or MLFQ)
+
+	// if (sched == PSJF)
+	//		sched_psjf();
+	// else if (sched == MLFQ)
+	// 		sched_mlfq();
+
+	// YOUR CODE HERE
+
+// - schedule policy
+#ifndef MLFQ
+	// Choose PSJF
+#else 
+	// Choose MLFQ
+#endif
+
+}
+
+/* Pre-emptive Shortest Job First (POLICY_PSJF) scheduling algorithm */
+static void sched_psjf() {
+	// - your own implementation of PSJF
+	// (feel free to modify arguments and return types)
+
+	// YOUR CODE HERE
+}
+
+
+/* Preemptive MLFQ scheduling algorithm */
+static void sched_mlfq() {
+	// - your own implementation of MLFQ
+	// (feel free to modify arguments and return types)
+
+	// YOUR CODE HERE
+}
+
+//DO NOT MODIFY THIS FUNCTION
+/* Function to print global statistics. Do not modify this function.*/
+void print_app_stats(void) {
+
+       fprintf(stderr, "Total context switches %ld \n", tot_cntx_switches);
+       fprintf(stderr, "Average turnaround time %lf \n", avg_turn_time);
+       fprintf(stderr, "Average response time  %lf \n", avg_resp_time);
+}
+
+
+// Feel free to add any other functions you need
+
+// YOUR CODE HERE
+
---
+++ Project2/code/thread-worker.h
@@ -0,0 +1,136 @@
+// File:	worker_t.h
+
+// List all group member's name:
+// username of iLab:
+// iLab Server:
+
+#ifndef WORKER_T_H
+#define WORKER_T_H
+
+#define _GNU_SOURCE
+
+/* To use Linux pthread Library in Benchmark, you have to comment the USE_WORKERS macro */
+#define USE_WORKERS 1
+
+/* include lib header files that you need here: */
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/ucontext.h>
+#include <ucontext.h>
+
+typedef uint worker_t;
+
+typedef struct TCB {
+	/* add important states in a thread control block */
+	/*
+	Thread ID
+	uint worker_t
+	*/
+	worker_t tid;
+
+	/*
+	Thread status
+	READY 0
+	SCHEDULED 1
+	BLOCKED 2
+	*/	
+	int status;	
+	
+	/*
+	Thread Context
+	The context of the thread
+	*/
+	ucontext_t ctx;
+	
+	/*
+	Thread stack
+	The stack for the thread
+	*/
+	void *stack;
+
+	/*
+	Thread priority
+	NUMPRIO 4
+	...
+	HIGH_PRIO 3
+	MEDIUM_PRIO 2
+	DEFAULT_PRIO 1
+	LOW_PRIO 0
+	*/
+	int priority;
+	// And more ...
+
+	// YOUR CODE HERE
+} tcb; 
+
+/* mutex struct definition */
+typedef struct worker_mutex_t {
+	/* add something here */
+
+	// YOUR CODE HERE
+} worker_mutex_t;
+
+/* Priority definitions */
+#define NUMPRIO 4
+
+#define HIGH_PRIO 3
+#define MEDIUM_PRIO 2
+#define DEFAULT_PRIO 1
+#define LOW_PRIO 0
+
+/* define your data structures here: */
+// Feel free to add your own auxiliary data structures (linked list or queue etc...)
+
+// YOUR CODE HERE
+
+
+/* Function Declarations: */
+
+/* create a new thread */
+int worker_create(worker_t * thread, pthread_attr_t * attr, void
+    *(*function)(void*), void * arg);
+
+/* give CPU pocession to other user level worker threads voluntarily */
+int worker_yield();
+
+/* terminate a thread */
+void worker_exit(void *value_ptr);
+
+/* wait for thread termination */
+int worker_join(worker_t thread, void **value_ptr);
+
+/* initial the mutex lock */
+int worker_mutex_init(worker_mutex_t *mutex, const pthread_mutexattr_t
+    *mutexattr);
+
+/* aquire the mutex lock */
+int worker_mutex_lock(worker_mutex_t *mutex);
+
+/* release the mutex lock */
+int worker_mutex_unlock(worker_mutex_t *mutex);
+
+/* destroy the mutex */
+int worker_mutex_destroy(worker_mutex_t *mutex);
+
+
+/* Function to print global statistics. Do not modify this function.*/
+void print_app_stats(void);
+
+#ifdef USE_WORKERS
+#define pthread_t worker_t
+#define pthread_mutex_t worker_mutex_t
+#define pthread_create worker_create
+#define pthread_exit worker_exit
+#define pthread_join worker_join
+#define pthread_mutex_init worker_mutex_init
+#define pthread_mutex_lock worker_mutex_lock
+#define pthread_mutex_unlock worker_mutex_unlock
+#define pthread_mutex_destroy worker_mutex_destroy
+#define pthread_setschedprio worker_setschedprio
+#endif
+
+#endif
